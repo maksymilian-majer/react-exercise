7606fba590225db9b6a629f934fba85b57b9f3a8
Maksymilian Majer <1215868+maksymilian-majer@users.noreply.github.com>
2019-04-09 18:40:52 +0200

    Applied thunk middleware

--- exercise/src/store/index.js
+++ exercise/src/store/index.js
@@ -1,7 +1,15 @@
-import { createStore } from 'redux';
-import { combineReducers } from "redux";
+import { createStore } from "redux";
+import { combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
 import reducers from "./reducers";
 
 const allReducers = combineReducers(reducers);
-export default createStore(allReducers, {});
+const initialState = {};
+const middleware = [thunk];
+
+export default createStore(
+  allReducers,
+  initialState,
+  compose(applyMiddleware(...middleware))
+);
